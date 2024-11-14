a639cfc8d41a7052905d172294b6855d0f114272
Nguyen Tat Dat <datnguyn1104@gmail.com>
2024-11-14 22:25:21 +0700

    Update bai1.cpp

--- Week9/bai1.cpp
+++ Week9/bai1.cpp
@@ -9,3 +9,38 @@ Các file được sắp xếp theo trật tự thời gian.
         + Người ta muốn sao lưu các file trong thư mục trên vào một ổ USB có kích thước 32G.
     Hãy thực hiện loại bỏ dần nếu cần một số file có kích thước nhỏ nhất để có thể thực hiện được thao tác trên.  
 */
+// Em xin lỗi cô, nhưng em chưa hiểu ý của đề bài ạ!
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Date {
+    int YYYY, MM, DD;
+};
+
+struct File {
+    string fileName;
+    long size;
+    Date date;
+    File* next;
+    
+    File(const string& name, long s, const Date& d)
+        : fileName(name), size(s), date(d), next(nullptr) {}
+};
+
+void hienThi(File* file) {
+    File* temp = file;
+    while (temp != nullptr) {
+        cout << "File Name: " << temp->fileName << endl;
+        cout << "File Size: " << temp->size << " bytes" << endl;
+        cout << "Date: " << temp->date.YYYY << "-" << temp->date.MM << "-" << temp->date.DD << endl;
+        temp = temp->next;
+    }
+}
+
+int main() {
+    File testFile("test.cpp", 1024, {2022, 1, 11});
+    hienThi(&testFile);
+    return 0;
+}
